6271905a10285e6ecde7645774bc6b73f36c4ee1
Jhonny Gil <gilsjhonny@gmail.com>
2017-03-29 23:14:57 -0400

    Update Bootstrap and fix Scrollspy

--- js/dist/scripts.js
+++ js/dist/scripts.js
@@ -1,41 +1,46 @@
 $(function () {
-  var $grid = $('.grid');
+    var $grid = $('.grid');
 
-  $grid.imagesLoaded(function () {
-    $grid.isotope({itemSelector: '.project-item', layoutMode: 'fitRows'});
-  });
+    $grid.imagesLoaded(function () {
+        $grid.isotope({
+            itemSelector: '.project-item',
+            layoutMode: 'fitRows',
+        });
+    });
 
-  // bind filter button click
-  $('#filters').on('click', 'button', function () {
-    console.log($(this));
-    var filterValue = $(this).attr('data-filter');
-    filterValue = filterValue;
-    $grid.isotope({filter: filterValue});
-  });
+    // bind filter button click
+    $('#filters').on('click', 'button', function () {
+        var filterValue = $(this).attr('data-filter');
+        filterValue = filterValue;
+        $grid.isotope({ filter: filterValue });
+    });
 
-  // find and switch checked button
-  var $buttonGroup = $('.button-group');
+    // find and switch checked button
+    var $buttonGroup = $('.button-group');
 
-  $buttonGroup.on('click', 'button', function () {
+    $buttonGroup.on('click', 'button', function() {
+        console.log($(this));
 
-    $buttonGroup
-      .find('.is-checked')
-      .removeClass('is-checked');
-    $(this).addClass('is-checked');
-  })
+        $buttonGroup.find('.is-checked').removeClass('is-checked');
+        $( this ).addClass('is-checked');
+    })
 
-  smoothScroll.init({speed: 1000, offset: -45});
+
+    smoothScroll.init({
+        speed: 1000,
+        offset: -53
+    });
 
 });
 
 $(window).scroll(function () {
 
-  var nav = $('.menu-header');
-  var scroll = $(window).scrollTop();
+    var nav = $('.navbar');
+    var scroll = $(window).scrollTop();
 
-  if (scroll >= 80) {
-    nav.addClass("nav-transform");
-  } else {
-    nav.removeClass("nav-transform");
-  }
+    if (scroll >= 80) {
+        nav.addClass("nav-transform");
+    } else {
+        nav.removeClass("nav-transform");
+    }
 });
